45f3386f43a8ced35d938fb408622e06a7dc07ea
Kyle Wiering <kyle.wiering@yahoo.com>
2015-12-17 01:47:55 -0600

    looking to move towards a newer approach, although much less convoluted.

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require_once  __DIR__ . DIRECTORY_SEPARATOR . 'vendor'. DIRECTORY_SEPARATOR . 'autoload.php';
 
+/** @var $Session Session */
 $Session = src\singleton\Session::inst();
+$Session->Start();
 
 ?>
---
+++ src/classes/Environment.php
@@ -0,0 +1,13 @@
+<?php
+namespace src\classes;
+class Environment
+{
+	const IS_LOADED = true;
+	const HTTP_COOKIE_DOMAIN 	= 'waryway.com';
+	const HTTPS_COOKIE_DOMAIN	= 'waryway.com';
+	const HTTP_COOKIE_PATH		= '/';
+	const HTTPS_COOKIE_PATH		= '/';
+	
+}
+
+?>
